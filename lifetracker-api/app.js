beec7eb52650f3b335e00533c4a99cb16f496db6
JoseMario3 <f.jose@wustl.edu>
2022-06-30 15:54:14 -0500

    Back-End login-register completed

---
+++ lifetracker-api/app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const authRoutes = require("./routes/auth");
+
+const { NotFoundError } = require("./utils/error");
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use("/auth", authRoutes);
+
+//psql -U postgres
+
+app.get("/", async(req, res) => {
+    console.log(123);
+    res.status(200).json({ ping: "pong" });
+});
+
+app.post("/", async(req, res) => {
+    let form = req.body;
+    console.log(form);
+    res.status(200).json({ form });
+});
+
+app.use((req, res, next) => {
+    return next(new NotFoundError());
+});
+
+app.use((err, req, res, next) => {
+    const status = err.status || 500;
+    const message = err.message;
+
+    return res.status(status).json({
+        error: { message, status },
+    });
+});
+
+module.exports = app;
